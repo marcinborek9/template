3520786d6463e3e83e969a3b47b945ab7471c83d
Marcin <marcinborek9@gmail.com>
2017-11-23 23:24:46 +0100

    dodanie animacji i menu

--- js/script.js
+++ js/script.js
@@ -104,6 +104,19 @@ $(document).ready(function(){
   $('.hamburger').on('click', function(){
     $('.menu').toggleClass('menu-hide');
 });
+
+$('a[href^="#"]').on('click',function (e) {
+  e.preventDefault();
+
+  var target = this.hash,
+  $target = $(target);
+
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 900, 'swing', function () {
+      window.location.hash = target;
+  });
+});
 });
 
 
