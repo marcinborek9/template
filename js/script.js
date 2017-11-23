53757fc7cde3c7433b8d503516665ac49fad72e8
Marcin <marcinborek9@gmail.com>
2017-11-23 17:54:26 +0100

    dodatkowe animacje

--- js/script.js
+++ js/script.js
@@ -49,4 +49,51 @@ new ScrollHandler('header-section');
 new ScrollHandler('gallery-section');
 new ScrollHandler('about-section');
 new ScrollHandler('projects-section');
+
+
+// FADEIN IMAGES ANIMATION ON SCROLL
+$(function(){ 
+  $('.monster').fadeIn('slow');
 });
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein-animation').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1500);
+            }
+        });
+    });
+});
+});
+
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+
